c764b83079fde2e9c2b12fc680a880cdd8c230c3
Vitoria S Alexandre <vitoria.alexandre290@gmail.com>
2023-05-30 20:42:47 -0300

    isola estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import store from "./store.js";
+import {adicionar, getItems} from "./store.js";
 
 const form = document.forms.entrada;
 form .addEventListener(`submit`, envia);
@@ -9,7 +9,7 @@ function envia(evento){
     evento.preventDefault();
     console.log('Formulário enviado');
     const  n = form.valor.value;
-    store.estado.push(n);
+    adicionar (n);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -18,9 +18,11 @@ function envia(evento){
 function atualiza(){
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    for(let i=0; i<store.estado.length; i++){
+    const items = getItems();
+    for(let i=0; i<items.length; i++){
         const li = document.createElement('li');
-        li.textContent = store.estado[i];
+        li.textContent = items[i];
         ol.appendChild(li);   
     }
+    items.push("Booom!");
 }
--- store.js
+++ store.js
@@ -1,2 +1,11 @@
 const store = {estado: [0,1,2,3] };
 export default store; 
+
+
+export function adicionar (valor){
+    store.estado.push(valor);
+}
+
+export function getItems(){
+    return [...store.estado];
+}
