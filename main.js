879f83d0b56ac53126e8636142dda6d65d70263a
Vitoria S Alexandre <vitoria.alexandre290@gmail.com>
2023-05-30 20:21:41 -0300

    Adiciona elementos no vetor

--- main.js
+++ main.js
@@ -8,11 +8,19 @@ atualiza();
 function envia(evento){
     evento.preventDefault();
     console.log('Formulário enviado');
-    store.estado++;
+    const  n = form.valor.value;
+    store.estado.push(n);
+    form.valor.value = "";
+    form.valor.focus();
     atualiza();
 }
 
 function atualiza(){
     const ol = document.querySelector('ol');
-    ol.innerHTML = `<li>${store.estado}</li>`;
+    ol.innerHTML = "";
+    for(let i=0; i<store.estado.length; i++){
+        const li = document.createElement('li');
+        li.textContent = store.estado[i];
+        ol.appendChild(li);   
+    }
 }
