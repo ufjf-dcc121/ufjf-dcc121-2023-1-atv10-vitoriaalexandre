8ad9dfe4dedf30a53f12451f4c27313b85ce2edf
Vitoria S Alexandre <vitoria.alexandre290@gmail.com>
2023-05-30 19:46:25 -0300

    adiciona estado externo e importações

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+import { estado } from "./store";
+
 const form = document.forms.entrada;
 form .addEventListener(`submit`, envia);
-
-let estado = 0;
 
 atualiza();
 
---
+++ store.js
@@ -0,0 +1,1 @@
+export let  estado = 0;
