7f32c66678fdac0565373a91ea463df6e0a75467
Vitoria S Alexandre <vitoria.alexandre290@gmail.com>
2023-05-30 19:27:32 -0300

    adiciona um estado basico

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const form = document.forms.entrada;
 form .addEventListener(`submit`, envia);
 
+let estado = 0;
+
 function envia(evento){
     evento.preventDefault();
     console.log('Formulário enviado');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
